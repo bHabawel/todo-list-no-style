028b5616fc6272bf77e8d72781f845e710fa5787
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-21 20:59:48 +0800

    added add, delete function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [listArray, setListArray] = useState([]);
+
+  function handlleAddList(list) {
+    setListArray((prevList) => [...prevList, list]);
+  }
+
+  function handleDeleteList(id) {
+    setListArray((prevList) => prevList.filter((list) => list.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddList onAddList={handlleAddList} />
+      <TodoList onShowList={listArray} onDeleteList={handleDeleteList} />
     </div>
   );
 }
 
+function Header() {
+  return (
+    <header className="header">
+      <h1>Todo List</h1>
+    </header>
+  );
+}
+
+function AddList({ onAddList }) {
+  const [addList, setAddList] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+
+    const newAddList = { item: addList, id: id };
+    onAddList(newAddList);
+    setAddList("");
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        placeholder="Add a task..."
+        value={addList}
+        onChange={(e) => setAddList(e.target.value)}
+      />{" "}
+      <button>Add</button>
+    </form>
+  );
+}
+
+function TodoList({ onShowList, onDeleteList }) {
+  return (
+    <main className="main">
+      <ul className="list-container" style={{ listStyle: "none" }}>
+        {onShowList.map((list) => (
+          <TodoContent
+            onShowList={list}
+            key={list.id}
+            onDeleteList={onDeleteList}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function TodoContent({ onShowList, onDeleteList }) {
+  return (
+    <>
+      <li className="list-item">{onShowList.item}</li>{" "}
+      <button
+        className="remove-button"
+        onClick={() => onDeleteList(onShowList.id)}
+      >
+        Remove
+      </button>
+    </>
+  );
+}
 export default App;
